815a0c501915cb625a7c1f6481703149c73c1068
David Bee <baeddavid@gmail.com>
2019-11-24 21:30:41 -0600

    add reading repository

---
+++ src/main/java/com/bae/readinglist/book/ReadingListRepository.java
@@ -0,0 +1,9 @@
+package com.bae.readinglist.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReadingListRepository  extends JpaRepository<Book, Long> {
+    List<Book> findByReader(String reader);
+}
